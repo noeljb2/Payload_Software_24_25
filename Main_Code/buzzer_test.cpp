5430e00dae12b71915d9dd7915b508786c2b5a61
noeljb1 <noeljb.2018@gmail.com>
2025-05-16 19:36:02 -0400

    Create buzzer_test.cpp

---
+++ Main_Code/buzzer_test.cpp
@@ -0,0 +1,83 @@
+//Noel Johnbosco 
+//5/14/2025
+
+
+//Payload Abstracted Overview 
+
+/*
+  Must power on, beep battery voltage, and beep a standby armed-and-ready mode
+  Must be able to receive GPS information and transmit it to your ground station (there will be a separate ground station for the payload)
+  Must be able to detect a successful nose cone separation (light sensor)
+  This is when the camera starts recording
+  Must be able to control the separation from the tether (pyro output to tender descender) [altitude based]
+  Must be able to control the deployment of the parachute (pyro output to line cutter) [altitude based]
+*/
+
+
+
+//Soar_24_25 Payload Computer Software
+#include <Wire.h>
+
+
+
+//Ground Station Pin def
+#define GPS_TX 0
+#define GPS_RX 1
+#define LORA_RESET 3
+#define BUZ 5
+#define TX_CAM 7
+#define RX_CAM 8
+#define LORA_BUSY 9
+#define LORA_CS 10
+#define LORA_MISO 12
+#define LORA_MOSI 11
+#define LORA_DIO1 24
+#define CURRENT_SENSE 26
+#define LIGHT_SENSER 27
+#define SOLENOID1 23
+#define SOLENOID2 22
+#define PAYLOAD_STATE 20
+#define CONT2 15
+#define CONT1 14
+
+//GPS serial
+#define mySerial Serial1 
+
+
+//LoRa def
+SX1262 radio = new Module(LORA_CS, LORA_DIO1, LORA_RESET, LORA_BUSY);
+
+
+//GPS def
+SFE_UBLOX_GNSS_SERIAL myGNSS;
+
+
+
+//Piezo Buzzer PWM Timer Setup
+TIM_TypeDef *Instance = (TIM_TypeDef *)pinmap_peripheral(digitalPinToPinName(BUZ), PinMap_PWM);
+uint32_t channel = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(BUZ), PinMap_PWM));
+HardwareTimer *MyTim = new HardwareTimer(Instance);
+
+//GNSS Values
+unsigned long lastGNSS = 0;                    
+long latitude = 0;
+long longitude = 0;
+long gnssAltitude = 0;
+byte SIV = 0;
+
+
+void setup() 
+{
+    Serial.begin(9600);
+
+
+
+}
+
+void loop() 
+{
+    MyTim->setPWM(channel, BUZ, 4000, 0); // 4kHz , 0% dutycycle
+    delay(50);
+    MyTim->setPWM(channel, BUZ, 4000, 50); // 4kHz, 10% dutycycle
+    delay(50);
+}
